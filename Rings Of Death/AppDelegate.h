6312c3e8f20c95fc90d3c9e88186a46f7b4b0a0f
Simon Booth <simon.booth@credit360.com>
2016-09-11 14:11:26 +0100

    Initial Commit

---
+++ Rings Of Death/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rings Of Death
+//
+//  Created by Simon Booth on 11/09/2016.
+//  Copyright © 2016 percysnoodle.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
